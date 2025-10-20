aa0ef8928c94497360891c336bd907f1789a1578
Mertcangndn <mertcangndn@gmail.com>
2025-10-20 13:41:05 +0300

    Örnek1 tamamlandı

--- Week3Lab.c
+++ Week3Lab.c
@@ -48,26 +48,46 @@ void removeNode(Node** head, int value){
     Node* tempNode = *head;
 
     while(tempNode != NULL){    //Node'lar bitmediği veya oluşturulduğu sürece
-        if((tempNode->value==value && undoTempNode==NULL) || (tempNode->value==value && tempNode->next==NULL)){ //İlk karakterle veya tek karakterle eşleşiyorsa
-            free(tempNode);
-            printf("\n\nNODE SİLİNDİ\n\n");
-            return;
-        }else if(tempNode->value == value){
-            undoTempNode->next = tempNode->next;
-            free(tempNode);
-            printf("\n\nNODE SİLİNDİ\n\n");
-            return;
+        if((tempNode->value==value)){   //Eşleşen karakter bulunduysa
+            if(undoTempNode==NULL && tempNode->next!=NULL){ //İlk karakterle eşleşiyorsa ve devamı var
+                *head = tempNode->next;
+                free(tempNode);
+                printf("\n\nNODE SİLİNDİ\n\n");
+                return;
+            }else if(undoTempNode==NULL && tempNode->next==NULL){ //tek karakterle eşleşiyorsa
+                *head = NULL;
+                printf("\n\nNODE SİLİNDİ\n\n");
+                return;
+            }else{
+                undoTempNode->next = tempNode->next;
+                free(tempNode);
+                printf("\n\nNODE SİLİNDİ\n\n");
+                return;
+            }
         }else{
             undoTempNode = tempNode;
             tempNode = tempNode->next;
+            continue;
         }
     }
 
     printf("\n\nNODE BULUNAMADI\n\n");
 }
 
+//Liste uzunluğunu yazdırma
+int count(Node* head){
+    int i = 0;
+    
+    while(head != NULL){
+        i++;
+        head = head->next;
+    }
+    
+    return i;
+}
+
 //Yazdırma Fonksiyonu
-void printNodes(Node* head){
+void printList(Node* head){
     printf("\n\n----------------------------\n");
     while(head!=NULL){
         printf("%d ",head->value);
@@ -75,6 +95,29 @@ void printNodes(Node* head){
     }
     printf("\n----------------------------\n\n");
     return;
+}
+
+//Listeyi serbest bırakma
+void clear(Node** head){
+    
+    if(*head != NULL){
+        Node* nextNode = (*head)->next;
+        while (*head!=NULL)
+        {
+            free(*head);
+            *head = nextNode;
+
+            if(nextNode != NULL){
+                nextNode = nextNode->next;
+            }else{
+                break;
+            }
+            
+        }
+    }
+
+    printf("\n\nNode'lar Temizlendi\n\n");
+    
 }
 
 void menu(void){
@@ -88,6 +131,8 @@ void menu(void){
         printf("[1] Node Değeri Girişi Yap\n");
         printf("[2] Node'ları Oku\n");
         printf("[3] Node Sil\n");
+        printf("[4] Node'ları Say\n");
+        printf("[5] Node'ları Temizle\n");
         printf("[0] Çıkış Yap\n\n\n");
         
         scanf("%d",&select);
@@ -99,16 +144,23 @@ void menu(void){
                 addOrdered(&x,value);
                 break;
             case 2:
-                printNodes(x);
+                printList(x);
                 break;
             case 3:
                 printf("Bir Value Girin: ");
                 scanf("%d",&value);
                 removeNode(&x,value);
                 break;
+            case 4:
+                printf("\n\nNode sayısı = %d",count(x));
+                printf("\n\n");
+                break;
+            case 5:
+                clear(&x);
+                break;
             case 0:
                 if(x!=NULL){
-                    while (x->next==NULL){
+                    while (x!=NULL && x->next==NULL){
                         Node* temp = x;
                         x = x->next;
                         free(temp);
