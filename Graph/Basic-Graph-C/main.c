ddd5606abce67246e55f37cc45491327f2c86279
Mertcangndn <mertcangndn@gmail.com>
2025-12-16 00:51:45 +0300

    Graflar hakkında çalışma yapıldı

---
+++ Graph/Basic-Graph-C/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Temel Node Yapısı
+typedef struct Node{
+    int value; //Bu hangi düğüm
+    struct Node* next;
+}Node;
+
+//Temel Graph Yapısı
+typedef struct Graph{
+    int totalNode; //Graftaki toplam düğüm sayısını tutan değişken
+    struct Node** adjLists; //Pointer dizisi (LinkedList'lerin başı)
+    int* visited;   //BFS/DFS arama yaparken ziyaret edilen node'ları tutar.
+}Graph;
+
+//Node Oluşurma Fonksiyonu
+Node* createNode(int value){
+    Node* newNode = malloc(sizeof(Node));
+    newNode->value=value;
+    newNode->next=NULL;
+    return newNode;
+}
+
+//Graph Oluşturma Fonksiyonu
+Graph* createGraph(int totalNode){
+    Graph* graph = malloc(sizeof(Graph));   //graph için yer açma
+    
+    graph->adjLists = malloc(totalNode * sizeof(Node)); //graph'ın adjlayer'i için yer açma (toplam node sayısı * 1 Node büyüküğü kdr)
+    graph->visited = malloc(totalNode * sizeof(int));   //graph'ın gezilenlari tutma listesi için yer açma (toplam node sayısı * 1 int büyüklüğü)
+    
+    //adjList'i ve visited listelerini doldurma
+    for(int i=0 ; i<totalNode ; i++){
+        graph->adjLists[i]=NULL; //Başlangıçta hiçbir node birbirine bağlı değil
+        graph->visited[i]=0; //Başlangıçta hiçbir node ziyaret edilmedi (gezilmedi)
+    }
+
+    return graph;
+}
+
+//Graph'a node ekleme fonksiyonu
+void addEdge(Graph* graph, int src, int dest){
+
+    // Kaynaktan Hedefe (src -> dest) bağ ekle
+    Node* newNode = createNode(dest);   //yeni node oluşturuldu (src eskiden vardı, dest oluşturuldu ve src'ye bağlanacak)
+    newNode->next = graph->adjLists[src];
+    graph->adjLists[src] = newNode;
+
+    //Eğer yönsüz graph ise tam tersini de eklemelisin. (dest -> src)
+    newNode = createNode(src);
+    newNode->next = graph->adjLists[dest];
+    graph->adjLists[dest] = newNode;
+}
+
+int main(void){
+    return 0;
+}
